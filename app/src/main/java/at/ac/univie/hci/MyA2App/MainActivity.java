4af14136b1c63d7f8520000b2ae0f5d08560ce8f
dexystorm <dexystormlp@gmail.com>
2025-04-13 12:33:46 +0200

    finished design more or less

--- app/src/main/java/at/ac/univie/hci/MyA2App/MainActivity.java
+++ app/src/main/java/at/ac/univie/hci/MyA2App/MainActivity.java
@@ -1,6 +1,13 @@
 package at.ac.univie.hci.MyA2App;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +28,40 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public String general_search = "";
+
+
+    public void filter_button_click(View view)
+    {
+        ImageButton filter_button = findViewById(R.id.filter_button);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popup = inflater.inflate(R.layout.filter_popup, null);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popup, width, height, focusable);
+        filter_button.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(filter_button, Gravity.TOP, 0, 0);
+            }
+        });
+
+        TextView go_button;
+        go_button = popup.findViewById(R.id.go_button);
+        go_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //ADD API STUFF HERE
+                popupWindow.dismiss();
+            }
+        });
+
+
+    }
+
 }
