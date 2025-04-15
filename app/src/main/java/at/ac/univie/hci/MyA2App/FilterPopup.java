8ba4a99a0fa94c37db2a65176e0df04beb9e690f
dexystorm <dexystormlp@gmail.com>
2025-04-15 16:41:20 +0200

    split in 2 files. but snackbar is in middle bug

---
+++ app/src/main/java/at/ac/univie/hci/MyA2App/FilterPopup.java
@@ -0,0 +1,123 @@
+package at.ac.univie.hci.MyA2App;
+
+import android.app.Activity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class FilterPopup
+{
+	private final MainActivity activity;
+	public PopupWindow filter_popup;
+
+
+	public FilterPopup(Activity activity)
+	{
+		this.activity = (MainActivity) activity;
+	}
+
+
+
+
+	public void show(View location)
+	{
+
+		LayoutInflater inflater = (LayoutInflater) activity.getSystemService(activity.LAYOUT_INFLATER_SERVICE);
+		View popup = inflater.inflate(R.layout.filter_popup, null);
+		filter_popup = new PopupWindow(popup, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+		filter_popup.showAtLocation(location, Gravity.TOP, 0, 0);
+
+
+
+		clear_button_clicked(popup, location);
+		go_button_clicked(popup, location);
+
+
+
+	}
+
+
+	private void go_button_clicked(View popup, View location)
+	{
+		TextView go_button;
+		go_button = popup.findViewById(R.id.go_button);
+		go_button.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				EditText artist_search = popup.findViewById(R.id.artist_search);
+				activity.artist_name = artist_search.getText().toString();
+				EditText art_search = popup.findViewById(R.id.art_search);
+				activity.art_name = art_search.getText().toString();
+				EditText country_search = popup.findViewById(R.id.country_search);
+				activity.country = country_search.getText().toString();
+				EditText time_from_search = popup.findViewById(R.id.time_period_from_search);
+				activity.time_period_from = time_from_search.getText().toString();
+				EditText time_to_search = popup.findViewById(R.id.time_period_to_search);
+				activity.time_period_to = time_to_search.getText().toString();
+				EditText medium_search = popup.findViewById(R.id.medium_search);
+				activity.medium = medium_search.getText().toString();
+
+				Snackbar.make(popup, "Searching...", Snackbar.LENGTH_SHORT).show();
+//				activity.call_api();
+//				popup.dismiss();
+			}
+		});
+	}
+
+
+	private void clear_button_clicked(View popup, View location)
+	{
+
+
+
+		Button clear_button = popup.findViewById(R.id.clear_button);
+		clear_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				EditText artist_search = popup.findViewById(R.id.artist_search);
+				artist_search.setText("");
+				activity.artist_name = "";
+
+				EditText art_search = popup.findViewById(R.id.art_search);
+				art_search.setText("");
+				activity.art_name = "";
+
+				EditText country_search = popup.findViewById(R.id.country_search);
+				country_search.setText("");
+				activity.country = "";
+
+				EditText time_from_search = popup.findViewById(R.id.time_period_from_search);
+				time_from_search.setText("");
+				activity.time_period_from = "";
+
+				EditText time_period_to_search = popup.findViewById(R.id.time_period_to_search);
+				time_period_to_search.setText("");
+				activity.time_period_to = "-1";
+
+				EditText medium_search = popup.findViewById(R.id.medium_search);
+				medium_search.setText("");
+				activity.medium = "";
+
+				//hides keybaord so that the snackbar can be visible
+				InputMethodManager imm = (InputMethodManager)activity.getSystemService(activity.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(popup.getWindowToken(), 0);
+
+				Snackbar.make(popup, "Filters have been cleared.", Snackbar.LENGTH_SHORT).show();
+
+			}
+		});
+	}
+}
--- app/src/main/java/at/ac/univie/hci/MyA2App/MainActivity.java
+++ app/src/main/java/at/ac/univie/hci/MyA2App/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String general_search = "";
-    public String artist_name  = "";
+    public String artist_name = "";
     public String art_name = "";
     public String country = "";
     public String time_period_from = "";
@@ -53,102 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void filter_button_click(View view)
-    {
-
-        ImageButton filter_button = findViewById(R.id.filter_button);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popup = inflater.inflate(R.layout.filter_popup, null);
-
-        int width = ViewGroup.LayoutParams.MATCH_PARENT;
-        int height = ViewGroup.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true;
-        PopupWindow popupWindow = new PopupWindow(popup, width, height, focusable);
-        filter_button.post(new Runnable() {
-            @Override
-            public void run() {
-                popupWindow.showAtLocation(filter_button, Gravity.TOP, 0, 0);
-            }
-        });
-
-
-        //GO BUTTON
-        TextView go_button;
-        go_button = popup.findViewById(R.id.go_button);
-        go_button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                EditText artist_search = popup.findViewById(R.id.artist_search);
-                artist_name = artist_search.getText().toString();
-                EditText art_search = popup.findViewById(R.id.art_search);
-                art_name = art_search.getText().toString();
-                EditText country_search = popup.findViewById(R.id.country_search);
-                country = country_search.getText().toString();
-                EditText time_from_serach = popup.findViewById(R.id.time_period_from_search);
-                time_period_from = time_from_serach.getText().toString();
-                EditText time_to_search = popup.findViewById(R.id.time_period_to_search);
-                time_period_to = time_to_search.getText().toString();
-                EditText medium_search = popup.findViewById(R.id.medium_search);
-                medium = medium_search.getText().toString();
-
-                Snackbar.make(view, "Searching...", Snackbar.LENGTH_SHORT).show();
-                call_api();
-                popupWindow.dismiss();
-            }
-        });
-
-
-
-
-
-
-
-        //CLEAR BUTTON
-        Button clear_button;
-        clear_button = popup.findViewById(R.id.clear_button);
-        clear_button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                EditText artist_search = popup.findViewById(R.id.artist_search);
-                artist_search.setText("");
-                artist_name  = "";
-
-                EditText art_search = popup.findViewById(R.id.art_search);
-                art_search.setText("");
-                art_name = "";
-
-                EditText country_search = popup.findViewById(R.id.country_search);
-                country_search.setText("");
-                country = "";
-
-                EditText time_from_search = popup.findViewById(R.id.time_period_from_search);
-                time_from_search.setText("");
-                time_period_from = "";
-
-                EditText time_period_to_search = popup.findViewById(R.id.time_period_to_search);
-                time_period_to_search.setText("");
-                time_period_to = "";
-
-                EditText medium_search = popup.findViewById(R.id.medium_search);
-                medium_search.setText("");
-                medium = "";
-
-                //hides keybaord so that the snackbar can be visible
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(popup.getWindowToken(), 0);
-
-                Snackbar.make(view, "Filters have been cleared.", Snackbar.LENGTH_SHORT).show();
-
-            }
-        });
+    public void filter_button_click(View view) {
+        FilterPopup popup = new FilterPopup(this);
+        popup.show(view);
 
 
     }
-
-
-
 }
