205d5c9e5cd0f01d21ae7705245eabf5d0e3bd3b
dexystorm <dexystormlp@gmail.com>
2025-04-20 11:06:55 +0200

    forgor to put the stuff in ArtowrkAdapter.java. now i've actually done it (still not finished tho)

---
+++ app/src/main/java/at/ac/univie/hci/MyA2App/ArtworkAdapter.java
@@ -0,0 +1,52 @@
+package at.ac.univie.hci.MyA2App;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class ArtworkAdapter extends ArrayAdapter<Artwork>
+{
+
+
+	private final Context context;
+	private final List<Artwork> artworks;
+
+	public ArtworkAdapter(Context context, List<Artwork> artworks)
+	{
+		super(context, 0, artworks);
+		this.context = context;
+		this.artworks = artworks;
+
+	}
+
+
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		if (convertView == null)
+		{
+			convertView = LayoutInflater.from(context).inflate(R.layout.artwork_list_item, parent, false);
+		}
+
+		Artwork artwork = artworks.get(position);
+
+		TextView titleView = convertView.findViewById(R.id.bp_title_text);
+		TextView artistView = convertView.findViewById(R.id.bp_artist_tex);
+		ImageView imageView = convertView.findViewById(R.id.artwork_image);
+
+		titleView.setText(artwork.title);
+		artistView.setText(artwork.artist_name);
+
+		// Image loading (replace this with real image URL logic)
+		String imageUrl = "https://www.artic.edu/iiif/2/" + artwork.alt_text + "/full/843,/0/default.jpg";
+		Glide.with(context).load(imageUrl).into(imageView);
+
+		return convertView;
+}
