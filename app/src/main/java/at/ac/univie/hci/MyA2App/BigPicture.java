b5f9d658d913e0e92819d8ce41c5d489b72f7d27
dexystorm <dexystormlp@gmail.com>
2025-04-20 15:46:31 +0200

    fixed all bugs for now. now need to fix startActivity(intent)

--- app/src/main/java/at/ac/univie/hci/MyA2App/BigPicture.java
+++ app/src/main/java/at/ac/univie/hci/MyA2App/BigPicture.java
@@ -22,22 +22,22 @@ public class BigPicture extends AppCompatActivity
 		setContentView(R.layout.big_picture);
 
 
-//		ImageView imageView = findViewById(R.id.pb_image_big);
-//		TextView titleView = findViewById(R.id.detail_title);
-//		TextView artistView = findViewById(R.id.detail_artist);
-//		TextView countryView = findViewById(R.id.detail_country);
-//		TextView yearView = findViewById(R.id.detail_year);
-//		TextView mediumView = findViewById(R.id.detail_medium);
-//		TextView descView = findViewById(R.id.detail_description);
-//
-//		Intent intent = getIntent();
-//		String imageUrl = intent.getStringExtra("imageUrl");
-//		String title = intent.getStringExtra("title");
-//		String artist = intent.getStringExtra("artist");
-//		String country = intent.getStringExtra("country");
-//		String year = intent.getStringExtra("year");
-//		String medium = intent.getStringExtra("medium");
-//		String description = intent.getStringExtra("description");
+		ImageView imageView = findViewById(R.id.bp_artwork_image);
+		TextView titleView = findViewById(R.id.bp_title_text);
+		TextView artistView = findViewById(R.id.bp_artist_text);
+		TextView countryView = findViewById(R.id.bp_country_text);
+		TextView yearView = findViewById(R.id.bp_year_text);
+		TextView mediumView = findViewById(R.id.bp_medium_text);
+		TextView descView = findViewById(R.id.bp_description_text);
+
+		Intent intent = getIntent();
+		String imageUrl = intent.getStringExtra("imageUrl");
+		String title = intent.getStringExtra("title");
+		String artist = intent.getStringExtra("artist");
+		String country = intent.getStringExtra("country");
+		String year = intent.getStringExtra("year");
+		String medium = intent.getStringExtra("medium");
+		String description = intent.getStringExtra("description");
 
 
 
